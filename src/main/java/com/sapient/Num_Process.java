049d183b2777c7b37f8e4cc45d57b08eb6b68a64
Boot-Error <booterror99@gmail.com>
2020-05-07 08:24:23 +0530

    week4 complete

--- src/main/java/com/sapient/Num_Process.java
+++ src/main/java/com/sapient/Num_Process.java
@@ -1,5 +1,7 @@
 package com.sapient;
 
+import java.util.*;
+
 /*
  * Num process module
  *
@@ -8,4 +10,36 @@ package com.sapient;
 interface numProcess {
 
 	public abstract int cal(int a, int b);
+}
+
+class calculator {
+
+	public static void run() {
+
+		numProcess add = (a, b) -> { return a + b; };
+		numProcess sub = (a, b) -> { return a - b; };
+		numProcess mul = (a, b) -> { return a * b; };
+		numProcess div = (a, b) -> { return a / b; };
+
+		numProcess[] opts = {add, sub, mul, div};
+
+		Scanner sc = new Scanner(System.in);
+
+		int a, b;
+		while (true) {
+
+			System.out.println("Enter 2 numbers: ");
+
+			a = sc.nextInt();
+			b = sc.nextInt();
+
+			System.out.println("Enter choice for computing: ");
+
+			int ch = sc.nextInt();
+
+			int res = opts[ch].cal(a, b);
+			System.out.println("The result is " + res);
+
+		}
+	}
 }
